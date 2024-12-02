de6b3b9c32ad8729d6bf6380adab3f20bed1d595
ZeroZeroOne <254635+ZeroZeroOne-dev@users.noreply.github.com>
2024-12-02 17:02:17 +0100

    2 in progress

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var lines = File.ReadAllLines("Input/data.txt");
+
+var left = new List<int>();
+var right = new List<int>();
+
+foreach (var line in lines)
+{
+    var split = line.Split("   ");
+    left.Add(int.Parse(split[0]));
+    right.Add(int.Parse(split[1]));
+}
+
+left.Sort();
+right.Sort();
+
+var dataLength = lines.Length;
+
+// part-1
+
+var distance = 0;
+for (int i = 0; i < dataLength;i++)
+{
+    distance += Math.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine($"Distance: {distance}");
+
+// part-2
+
+var similarity = 0;
+for (int i = 0; i < dataLength;i++)
+{
+    var current = left[i];
+    similarity += current * right.Count(c => c == current);
+}
+
+Console.WriteLine($"Similarity: {similarity}");
---
+++ 2/Program.cs
@@ -0,0 +1,63 @@
+﻿var levels = File.ReadAllLines("Input/data.txt").Select(strReport => strReport.Split(' ').Select(level => int.Parse(level)).ToArray());
+
+var safeLevels = levels.Where(IsSave);
+
+var safe = 0;
+// foreach (var level in levels)
+// {
+//     var increase = 0;
+//     var decrease = 0;
+
+//     for (int i = 0; i < level.Length; i++)
+//     {
+//         if (i == level.Length - 1)
+//         {
+//             safe++;
+//             continue;
+//         }
+
+//         var difference = level[i] - level[i + 1];
+//         if (difference < 0)
+//         {
+//             increase++;
+//         }
+//         else if (difference > 0)
+//         {
+//             decrease++;
+//         }
+//         else
+//         {
+//             break;
+//         }
+
+//         if (increase != 0 && decrease != 0)
+//         {
+//             break;
+//         }
+
+//         var absDifference = Math.Abs(difference);
+//         if (absDifference < 1 || absDifference > 3)
+//         {
+//             break;
+//         }
+//     }
+// }
+
+foreach (var level in levels)
+{
+
+}
+
+static bool IsSave(int[] level)
+{
+    var pairs = new List<(int first, int second)>();
+
+    for (int i = 0; i < level.Length - 1; i++)
+    {
+        pairs.Add((level[i], level[i+1]));
+    }
+
+    return false;
+}
+
+Console.WriteLine($"Safe: {safe}");
