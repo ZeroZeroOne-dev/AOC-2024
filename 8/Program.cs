023dd7b066e6a9a1bac9ea3394f10b1a71138326
ZeroZeroOne-dev <github@zerozeroone.dev>
2024-12-08 18:49:11 +0100

    test working

---
+++ 8/Program.cs
@@ -0,0 +1,30 @@
+﻿// make one dimensional array
+// use dif of a and b to calculate new coordinate
+
+var lines = File.ReadAllLines("Input/data.txt");
+
+var originalSpan = lines.First().Length;
+var rowSpan = originalSpan;
+
+List<char> padded = [
+    ..lines.SelectMany(l => l),
+];
+
+var index1 = padded.IndexOf('a');
+var index2 = padded.LastIndexOf('a');
+
+var indexDiff = index2 - index1;
+
+var t1 = index1 - indexDiff;
+
+var t1X = t1 % rowSpan;
+var t1Y = t1 / rowSpan;
+
+Console.WriteLine($"t1X: {t1X}, t1Y: {t1Y}");
+
+var t2 = index2 + indexDiff;
+
+var t2X = t2 % rowSpan;
+var t2Y = t2 / rowSpan;
+
+Console.WriteLine($"t2X: {t2X}, t2Y: {t2Y}");
